e3ad46900755027ef7181b14fde8b4076ff08162
Valentin Radu <radu.v.valentin@me.com>
2022-04-17 14:17:37 +0100

    Reversed model object to model key

--- Sources/Trellis/Store.swift
+++ Sources/Trellis/Store.swift
@@ -7,12 +7,21 @@
 
 import Foundation
 
+private struct ModelKey: EnvironmentKey {
+    static var defaultValue: Any? = nil
+}
+
 private typealias AnyMutation = (Any, any Action) async throws -> Void
 private struct MutationsKey: EnvironmentKey {
     static var defaultValue: [AnyMutation] = []
 }
 
 private extension EnvironmentValues {
+    var model: Any? {
+        get { self[ModelKey.self] }
+        set { self[ModelKey.self] = newValue }
+    }
+
     var mutations: [AnyMutation] {
         get { self[MutationsKey.self] }
         set { self[MutationsKey.self] = newValue }
@@ -22,38 +31,36 @@ private extension EnvironmentValues {
 public typealias Mutation<S, A> = (S, A, Send) async throws -> Void where A: Action
 
 public extension Service {
-    func with<M>(model: M) -> some Service
-        where M: AnyObject
-    {
-        environmentObject(model)
+    func with<S>(model: S) -> some Service {
+        environment(\.model, value: model)
     }
 }
 
-public struct Store<M>: Service
-where M: AnyObject
-{
-    @EnvironmentObject private var _model: M
+public struct Store<S>: Service {
+    @Environment(\.model) private var _model
     @Environment(\.mutations) private var _mutations
     @Environment(\.send) private var _send
 
-    public init(model _: M.Type) {}
+    public init(model _: S.Type) {}
 
     public var body: some Service {
         EmptyService()
             .observe {
                 for receiver in _mutations {
-                    try await receiver(_model, $0)
+                    if let model = _model {
+                        try await receiver(model, $0)
+                    }
                 }
             }
     }
 
     public func mutate<A>(on _: A.Type,
-                          _ closure: @escaping Mutation<M, A>) -> some Service
+                          _ closure: @escaping Mutation<S, A>) -> some Service
         where A: Action
     {
         let receiver: AnyMutation = {
             if let action = $1 as? A,
-               let store = $0 as? M
+               let store = $0 as? S
             {
                 try await closure(store, action, _send)
             }
